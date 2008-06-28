47231050a544241203425f579fb4cd36a1776b5c
Jim Meyering <meyering@redhat.com>
2008-06-28 18:19:17 +0200

    build-related revamp
    * Makefile.am (ACLOCAL_AMFLAGS): Define to "-I m4".
    * bootstrap (modules): Add gitlog-to-changelog, gnu-make, posix-shell.
    * configure.ac: Use gnulib's gl_EARLY and gl_INIT.
    * tests/Makefile.am (TESTS_ENVIRONMENT): Move definition...
    * tests/check.mk: ...to here.  New file, based on coreutils'.
    * build-aux/check.mk: New file, from coreutils.

---
+++ tests/check.mk
@@ -0,0 +1,32 @@
+# Include this file at the end of each tests/*/Makefile.am.
+# Copyright (C) 2007, 2008 Free Software Foundation, Inc.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Propagate build-related Makefile variables to test scripts.
+TESTS_ENVIRONMENT = \
+  top_srcdir=$(top_srcdir) \
+  srcdir=$(srcdir) \
+  PATCH="$(PATCH)" \
+  PERL="$(PERL)" \
+  perllibdir="`$(am__cd) $(top_srcdir) && pwd`" \
+  PATH="$(VG_PATH_PREFIX)`pwd`/..$(PATH_SEPARATOR)$$PATH"
+
+TEST_LOGS = $(TESTS:=.log)
+
+# Parallel replacement of Automake's check-TESTS target.
+SUFFIXES =
+include $(top_srcdir)/build-aux/check.mk
+
+VERBOSE = yes
