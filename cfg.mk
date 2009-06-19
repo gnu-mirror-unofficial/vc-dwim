5259cca2db111a979eb1662bbefbe6d6286cc0e5
Jim Meyering <meyering@redhat.com>
2009-06-20 00:08:02 +0200

    tests: use Exit consistently

--- cfg.mk
+++ cfg.mk
@@ -33,9 +33,8 @@ gpg_key_ID = B9AB9A16
 # at the top of the file for each `make distcheck' run.
 local-checks-to-skip = strftime-check patch-check check-AUTHORS
 
-# The local directory containing the checked-out copy of gnulib used in this
-# release.  Used solely to get gnulib's SHA1 for the "announcement" target.
-gnulib_dir = /gnulib
+# We define Exit in a different file.
+Exit_witness_file = tests/trap-setup
 
 # Now that we have better tests, make this the default.
 export VERBOSE = yes
