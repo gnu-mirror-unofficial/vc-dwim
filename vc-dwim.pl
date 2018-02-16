5916cacd6a2e895fa683c101f0a1185311109950
Jim Meyering <meyering@fb.com>
2018-02-16 16:48:52 +1300

    doc: replace http:// with https:// everywhere
    Induce this change by running this command:
    git grep -l 'http://www\.gnu\.org/licenses' \
      |xargs perl -pi -e 's,\bhttp(://www\.gnu\.org/licenses)\b,https$1,'

--- vc-dwim.pl
+++ vc-dwim.pl
@@ -16,7 +16,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # Given a command like vc-dwim ChangeLog lib/ChangeLog..., check that each
 # ChangeLog has been modified, determine the list of affected files from
